516ac47d61536682943136fc6bc7e7e7a21175a0
Pedram <tadayoni@gmail.com>
2019-07-16 21:19:22 -0700

    Fix vpc name

--- modules/network/resource.tf
+++ modules/network/resource.tf
@@ -97,7 +97,7 @@ resource "aws_nat_gateway" "ngw2" {
 
 # Route Table
 resource "aws_route_table" "public_rt" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -119,7 +119,7 @@ resource "aws_route_table_association" "public_rta2" {
 }
 
 resource "aws_route_table" "private_rt_1" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -137,7 +137,7 @@ resource "aws_route_table_association" "private_rta1" {
 }
 
 resource "aws_route_table" "private_rt_2" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
