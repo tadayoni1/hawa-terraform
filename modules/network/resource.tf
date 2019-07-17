aea406b77d32f32d3a8569271a34f1c37675c5c7
Pedram <tadayoni@gmail.com>
2019-07-16 18:55:08 -0700

    Add internet gateway

--- modules/network/resource.tf
+++ modules/network/resource.tf
@@ -7,3 +7,12 @@ resource "aws_vpc" "main" {
     Name = "${var.EnvironmentName}-vpc"
   }
 }
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "${var.EnvironmentName}-vpc"
+  }
+}
