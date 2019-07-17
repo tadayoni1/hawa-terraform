86f58bd91549679067812f5cb42d6ecb285a855d
Pedram <tadayoni@gmail.com>
2019-07-16 21:17:59 -0700

    Fix duplicate names

--- modules/network/resource.tf
+++ modules/network/resource.tf
@@ -109,11 +109,11 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table_association" "rta1" {
+resource "aws_route_table_association" "public_rta1" {
   subnet_id      = "${aws_subnet.public_subnet_1.id}"
   route_table_id = "${aws_route_table.public_rt.id}"
 }
-resource "aws_route_table_association" "rta2" {
+resource "aws_route_table_association" "public_rta2" {
   subnet_id      = "${aws_subnet.public_subnet_2.id}"
   route_table_id = "${aws_route_table.public_rt.id}"
 }
@@ -131,7 +131,7 @@ resource "aws_route_table" "private_rt_1" {
   }
 }
 
-resource "aws_route_table_association" "rta1" {
+resource "aws_route_table_association" "private_rta1" {
   subnet_id      = "${aws_subnet.private_subnet_1.id}"
   route_table_id = "${aws_route_table.private_rt.id}"
 }
@@ -149,7 +149,7 @@ resource "aws_route_table" "private_rt_2" {
   }
 }
 
-resource "aws_route_table_association" "rta2" {
+resource "aws_route_table_association" "private_rta2" {
   subnet_id      = "${aws_subnet.private_subnet_2.id}"
   route_table_id = "${aws_route_table.private_rt.id}"
 }
