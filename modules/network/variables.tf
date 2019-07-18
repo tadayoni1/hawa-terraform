f4266ec585bbb349ec4cbab17d93bfb0cc87d346
Pedram <tadayoni@gmail.com>
2019-07-17 19:43:03 -0700

    Add missing parameters

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,9 +1,15 @@
 # General 
 variable EnvironmentName {}
 variable aws_region {}
+
+# IAM
+variable "S3BucketName" {}
+variable "EC2InstanceRole" {}
+
 # VPC
 variable EnableDnsHostnames {}
 variable VpcCIDR {}
+
 # Subnets
 variable PublicSubnet1CIDR {}
 variable PublicSubnet2CIDR {}
--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,6 +1,17 @@
 # General 
 variable EnvironmentName {}
+variable aws_region {}
 
 # IAM
 variable "S3BucketName" {}
 variable "EC2InstanceRole" {}
+
+# VPC
+variable EnableDnsHostnames {}
+variable VpcCIDR {}
+
+# Subnets
+variable PublicSubnet1CIDR {}
+variable PublicSubnet2CIDR {}
+variable PrivateSubnet1CIDR {}
+variable PrivateSubnet2CIDR {}
