be0c28c5ca6493951b889e8c976b95c09f27eb42
Pedram <tadayoni@gmail.com>
2019-07-18 20:29:58 -0700

    Remove extra modules from asg resource.tf

--- modules/services/main.tf
+++ modules/services/main.tf
@@ -1,20 +1,3 @@
-module "iam" {
-    source = "./iam/"
-
-    EnvironmentName             = "${var.EnvironmentName}"
-    S3BucketName                = "${var.S3BucketName}"
-    EC2InstanceRole             = "${var.EC2InstanceRole}"
-}
-
-module "sg" {
-    source= "./sg/"
-
-    EnvironmentName             = "${var.EnvironmentName}"
-    VPC_ID                      = "${var.VPC_ID}"
-
-}
-
-
 module "asg" {
     source = "./asg/"
 
