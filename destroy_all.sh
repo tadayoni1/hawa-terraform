3f39201c24ca2335ca2be8e1ae5e98a9a1e84f81
Pedram <tadayoni@gmail.com>
2019-07-21 00:00:47 -0700

    Add auto approve to destroy_all script

--- destroy_all.sh
+++ destroy_all.sh
@@ -20,7 +20,7 @@ popd
 pushd modules/services
 terraform init -backend-config="key=state/services/hawa_backend_config"
 
-terraform destroy -var-file="../../parameters.tfvars" \
+terraform destroy -auto-approve -var-file="../../parameters.tfvars" \
 -var="web_server_sg_id=$web_server_sg_id" -var="lb_sg_id=$lb_sg_id" \
 -var="VPC_ID=$vpc_id" \
 -var="public_subnet_1_id=$public_subnet_1_id" -var="public_subnet_2_id=$public_subnet_2_id" \
@@ -30,11 +30,12 @@ popd
 pushd modules/security
 terraform init -backend-config="key=state/security/hawa_backend_config"
 
-terraform destroy -var-file="../../parameters.tfvars" -var="VPC_ID=$vpc_id"
+terraform destroy -auto-approve -var-file="../../parameters.tfvars" -var="VPC_ID=$vpc_id"
 popd
 
 pushd modules/network
 terraform init -backend-config="key=state/network/hawa_backend_config"
 
+terraform destroy -auto-approve -var-file="../../parameters.tfvars"
 popd
 
