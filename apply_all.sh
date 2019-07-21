2ef52861734b65b5ff156df39a34c11d1cb3f2e6
Pedram <tadayoni@gmail.com>
2019-07-20 23:50:49 -0700

    Add auto approve to apply_all script

--- apply_all.sh
+++ apply_all.sh
@@ -3,7 +3,7 @@
 pushd modules/network
 terraform init -backend-config="key=state/network/hawa_backend_config"
 
-terraform apply -var-file="../../parameters.tfvars"
+terraform apply -auto-approve -var-file="../../parameters.tfvars"
 
 vpc_id=$(terraform output "VpcID")
 public_subnet_1_id=$(terraform output "public_subnet_1_id")
@@ -15,7 +15,7 @@ popd
 pushd modules/security
 terraform init -backend-config="key=state/security/hawa_backend_config"
 
-terraform apply -var-file="../../parameters.tfvars" -var="VPC_ID=$vpc_id"
+terraform apply -auto-approve -var-file="../../parameters.tfvars" -var="VPC_ID=$vpc_id"
 
 web_server_sg_id=$(terraform output "web_server_sg_id")
 lb_sg_id=$(terraform output "lb_sg_id")
@@ -24,7 +24,7 @@ popd
 pushd modules/services
 terraform init -backend-config="key=state/services/hawa_backend_config"
 
-terraform apply -var-file="../../parameters.tfvars" \
+terraform apply -auto-approve -var-file="../../parameters.tfvars" \
 -var="web_server_sg_id=$web_server_sg_id" -var="lb_sg_id=$lb_sg_id" \
 -var="VPC_ID=$vpc_id" \
 -var="public_subnet_1_id=$public_subnet_1_id" -var="public_subnet_2_id=$public_subnet_2_id" \
